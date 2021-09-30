2cc03aac5dad1c58d9e86d886e90ab2e172b4331
MaxCorvus <83407700+MaxCorvus@users.noreply.github.com>
2021-09-30 20:55:29 +0300

    add some files

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+echo "Hello World!";
+require_once 'vendor/autoload.php';
+
+
+$blabla = [
+  'lal' => 'alall',
+  'sfdf' => 'f',
+  'dfd' => [
+      'dfd' => 'dfdf',
+      'dfd1' => 'dfdf',
+      'dfd2' => 'dfdf',
+      'dfd3' => 'dfdf',
+      'dfd4' => 'dfdf',
+      'dfd5' => 'dfdf',
+      'dfd6' => 'dfdf',
+      'dfd7' => 'dfdf',
+      'dfd8' => 'dfdf',
+      'df9' => 'dfdf',
+      'dfd10' => 'dfdf',
+      'dfd11' => 'dfdf',
+      'dfd12' => 'dfdf',
+      'dfd13' => 'dfdf',
+
+  ]
+];
+dump($blabla);
+var_dump($blabla);
+
